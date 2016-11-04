df2fda8c0ca2dc7b169a7c66225bea27e3134c0e
Alexander Gabriel <alex@gabriel-software.ch>
2016-11-04 10:33:18 +0100

    remove unneeded code

--- app/main.development.js
+++ app/main.development.js
@@ -7,64 +7,6 @@ let mainWindow = null
 const saveConfigValue = require('./src/saveConfigValue.js')
 const getConfigSync = require('./src/getConfigSync.js')
 
-// this was used for installer
-// uncommented because of problems in release
-/*
-const handleStartupEvent = () => {
-  if (process.platform !== 'win32') {
-    return false
-  }
-
-  function exeSquirrelCommand(args, cb) {
-    const updateDotExe = path.resolve(path.dirname(process.execPath), '..', 'update.exe')
-    const child = childProcess.spawn(updateDotExe, args, { detached: true })
-    child.on('close', () => cb())
-  }
-
-  function install(cb) {
-    const target = path.basename(process.execPath)
-    exeSquirrelCommand(['--createShortcut', target], cb)
-  }
-
-  const squirrelCommand = process.argv[1]
-  switch (squirrelCommand) {
-    case '--squirrel-install':
-      install(app.quit())
-      break
-    case '--squirrel-updated':
-
-      // Optionally do things such as:
-      //
-      // - Install desktop and start menu shortcuts
-      // - Add your .exe to the PATH
-      // - Write to the registry for things like file associations and
-      //   explorer context menus
-
-      // Always quit when done
-      app.quit()
-
-      return true
-    case '--squirrel-uninstall':
-      // Undo anything you did in the --squirrel-install and
-      // --squirrel-updated handlers
-
-      // Always quit when done
-      app.quit()
-
-      return true
-    case '--squirrel-obsolete':
-      // This is called on the outgoing version of your app before
-      // we update to the new version - it's the opposite of
-      // --squirrel-updated
-      app.quit()
-      return true
-  }
-}
-
-if (handleStartupEvent()) {
-  return
-}*/
-
 if (process.env.NODE_ENV === 'production') {
   const sourceMapSupport = require('source-map-support') // eslint-disable-line
   sourceMapSupport.install()
