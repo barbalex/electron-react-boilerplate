02665565497361a375a2a37cad66d36f4fad84bf
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-14 18:11:47 +0100

    style

--- app/components/geschaeft/AreaParlVorstoss.js
+++ app/components/geschaeft/AreaParlVorstoss.js
@@ -8,6 +8,104 @@ import regularStyles from './areaParlVorstoss.css'
 import pdfStyles from './areaParlVorstossPdf.css'
 import createOptions from '../../src/createOptions'
 
+const parlVorstossStufe = ({ isPrintPreview, geschaeft, change, nrOfFieldsBeforePv }) => {
+  if (!isPrintPreview) {
+    return (
+      <div>
+        <ControlLabel>
+          Stufe
+        </ControlLabel>
+        <Radio
+          data-value="1"
+          checked={geschaeft.parlVorstossStufe === '1'}
+          onChange={change}
+          bsSize="small"
+          name="parlVorstossStufe"
+          tabIndex={2 + nrOfFieldsBeforePv}
+        >
+          1: nicht überwiesen
+        </Radio>
+        <Radio
+          data-value="2"
+          checked={geschaeft.parlVorstossStufe === '2'}
+          name="parlVorstossStufe"
+          onChange={change}
+          bsSize="small"
+          tabIndex={3 + nrOfFieldsBeforePv}
+        >
+          2: überwiesen
+        </Radio>
+      </div>
+    )
+  }
+  let value = ''
+  if (geschaeft.parlVorstossStufe === '1') value = '1: nicht überwiesen'
+  if (geschaeft.parlVorstossStufe === '2') value = '2: überwiesen'
+  return (
+    <div>
+      <ControlLabel>
+        Stufe
+      </ControlLabel>
+      <FormControl
+        type="text"
+        value={value}
+        bsSize="small"
+        tabIndex={2 + nrOfFieldsBeforePv}
+      />
+    </div>
+  )
+}
+
+const parlVorstossZustaendigkeit = ({ isPrintPreview, geschaeft, change, nrOfFieldsBeforePv }) => {
+  if (!isPrintPreview) {
+    return (
+      <div>
+        <ControlLabel>
+          Zuständigkeit
+        </ControlLabel>
+        <Radio
+          data-value="hauptzuständig"
+          checked={geschaeft.parlVorstossZustaendigkeitAwel === 'hauptzuständig'}
+          name="parlVorstossZustaendigkeitAwel"
+          onChange={change}
+          bsSize="small"
+          tabIndex={6 + nrOfFieldsBeforePv}
+        >
+          haupt
+        </Radio>
+        <Radio
+          data-value="mitberichtzuständig"
+          checked={geschaeft.parlVorstossZustaendigkeitAwel === 'mitberichtzuständig'}
+          name="parlVorstossZustaendigkeitAwel"
+          onChange={change}
+          bsSize="small"
+          tabIndex={7 + nrOfFieldsBeforePv}
+        >
+          mitbericht
+        </Radio>
+      </div>
+    )
+  }
+  const value = (
+    geschaeft.parlVorstossZustaendigkeitAwel ?
+    geschaeft.parlVorstossZustaendigkeitAwel.replace('zuständig', '') :
+    ''
+  )
+  return (
+    <div>
+      <ControlLabel>
+        Zuständigkeit
+      </ControlLabel>
+      <FormControl
+        type="text"
+        value={value}
+        bsSize="small"
+        tabIndex={6 + nrOfFieldsBeforePv}
+      />
+    </div>
+  )
+}
+
 const AreaParlVorstoss = ({
   geschaeft,
   parlVorstossTypOptions,
@@ -38,54 +136,10 @@ const AreaParlVorstoss = ({
         </FormControl>
       </div>
       <div className={styles.fieldStufe}>
-        <ControlLabel>
-          Stufe
-        </ControlLabel>
-        <Radio
-          data-value="1"
-          checked={geschaeft.parlVorstossStufe === '1'}
-          onChange={change}
-          bsSize="small"
-          name="parlVorstossStufe"
-          tabIndex={2 + nrOfFieldsBeforePv}
-        >
-          1: nicht überwiesen
-        </Radio>
-        <Radio
-          data-value="2"
-          checked={geschaeft.parlVorstossStufe === '2'}
-          name="parlVorstossStufe"
-          onChange={change}
-          bsSize="small"
-          tabIndex={3 + nrOfFieldsBeforePv}
-        >
-          2: überwiesen
-        </Radio>
+        {parlVorstossStufe({ isPrintPreview, geschaeft, change, nrOfFieldsBeforePv })}
       </div>
       <div className={styles.fieldZustaendigkeit}>
-        <ControlLabel>
-          Zuständigkeit
-        </ControlLabel>
-        <Radio
-          data-value="hauptzuständig"
-          checked={geschaeft.parlVorstossZustaendigkeitAwel === 'hauptzuständig'}
-          name="parlVorstossZustaendigkeitAwel"
-          onChange={change}
-          bsSize="small"
-          tabIndex={6 + nrOfFieldsBeforePv}
-        >
-          haupt
-        </Radio>
-        <Radio
-          data-value="mitberichtzuständig"
-          checked={geschaeft.parlVorstossZustaendigkeitAwel === 'mitberichtzuständig'}
-          name="parlVorstossZustaendigkeitAwel"
-          onChange={change}
-          bsSize="small"
-          tabIndex={7 + nrOfFieldsBeforePv}
-        >
-          mitbericht
-        </Radio>
+        {parlVorstossZustaendigkeit({ isPrintPreview, geschaeft, change, nrOfFieldsBeforePv })}
       </div>
     </div>
   )
