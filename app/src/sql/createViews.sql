f52efe12f30c6a14ece4565e0e6c26a32dd54fae
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-14 15:17:31 +0100

    remove v_geschaefte_kontakteIntern

--- app/src/sql/createViews.sql
+++ app/src/sql/createViews.sql
@@ -12,33 +12,15 @@ AS
       ON geschaefteKontakteExtern.idKontakt = externe.id
   GROUP BY geschaefteKontakteExtern.idGeschaeft;
 
-DROP VIEW IF EXISTS v_geschaefte_kontakteIntern;
-CREATE VIEW
-  v_geschaefte_kontakteIntern
-AS
-  SELECT
-    geschaefteKontakteIntern.idGeschaeft AS idGeschaeft,
-    GROUP_CONCAT(interne.vorname || ' ' || interne.name, ", ") AS kontaktInternVornameName
-  FROM
-    interne
-    INNER JOIN
-      geschaefteKontakteIntern
-      ON geschaefteKontakteIntern.idKontakt = interne.id
-  GROUP BY geschaefteKontakteIntern.idGeschaeft;
-
 DROP VIEW IF EXISTS v_geschaefte;
 CREATE VIEW
   v_geschaefte
 AS
   SELECT
     geschaefte.*,
-    v_geschaefte_kontakteIntern.kontaktInternVornameName,
     v_geschaefte_kontakteExtern.kontaktExternNameVorname
   FROM
-    ((geschaefte
-    LEFT JOIN
-      v_geschaefte_kontakteIntern
-      ON geschaefte.idGeschaeft = v_geschaefte_kontakteIntern.idGeschaeft)
+    (geschaefte
     LEFT JOIN
       v_geschaefte_kontakteExtern
       ON geschaefte.idGeschaeft = v_geschaefte_kontakteExtern.idGeschaeft)
