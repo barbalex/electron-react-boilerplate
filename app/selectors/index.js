c0a00fff7b0c3796dca8724f40029ff109fcfdcd
Alexander Gabriel <alex@gabriel-software.ch>
2017-01-15 14:53:03 +0100

    work on selectors

--- app/selectors/index.js
+++ app/selectors/index.js
@@ -10,16 +10,24 @@ const getGeko = (state) => state.geschaefte.geko
 
 export const getGeschaefteWithNSideData = createSelector(
   [getGeschaefte, getGki, getGke, getInterne, getExterne, getGeko],
-  (geschaefteOrig, gki, gke, interne, externe, geko) => {
-    // attach all arrays to each geschaft
-    const newGeschaefte = []
-    geschaefte.forEach((g) => {
+  (geschaefteInState, gki, gke, interne, externe, geko) => {
+    // attach all arrays to each geschaeft
+    const geschaefte = geschaefteInState.map((g) => {
       const newGeschaeft = clone(g)
       newGeschaeft.interne = gki
         .filter(i => i.idGeschaeft === g.idGeschaeft)
         .map((gk) =>
           interne.find(i => i.id === gk.idKontakt) || null
         )
+      newGeschaeft.externe = gke
+        .filter(i => i.idGeschaeft === g.idGeschaeft)
+        .map((gk) =>
+          externe.find(i => i.id === gk.idKontakt) || null
+        )
+      newGeschaeft.geko = geko
+        .filter(gko => gko.idGeschaeft === g.idGeschaeft)
+      return newGeschaeft
     })
+    return geschaefte
   }
 )
