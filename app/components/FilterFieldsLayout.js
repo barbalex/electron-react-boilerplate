6c881b1ba17ab0c756b137a40efcc7d5393d357a
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-12 15:22:24 +0100

    Create function

--- app/components/FilterFieldsLayout.js
+++ app/components/FilterFieldsLayout.js
@@ -1,31 +1,25 @@
-import React, { Component, PropTypes } from 'react'
+import React, { PropTypes } from 'react'
 import SplitPane from 'react-split-pane'
 import FilterFields from '../containers/filterFields/FilterFields'
 import Geschaefte from '../containers/Geschaefte'
 
-class FilterFieldsLayout extends Component {
-  static propTypes = {
-    config: PropTypes.object.isRequired,
-    configSetKey: PropTypes.func.isRequired,
-  }
+const FilterFieldsLayout = ({
+  configSetKey,
+  config,
+}) =>
+  <SplitPane
+    split="vertical"
+    minSize={100}
+    defaultSize={config.geschaefteColumnWidth}
+    onChange={size => configSetKey('geschaefteColumnWidth', size)}
+  >
+    <Geschaefte />
+    <FilterFields />
+  </SplitPane>
 
-  render = () => {
-    const {
-      configSetKey,
-      config,
-    } = this.props
-    return (
-      <SplitPane
-        split="vertical"
-        minSize={100}
-        defaultSize={config.geschaefteColumnWidth}
-        onChange={size => configSetKey('geschaefteColumnWidth', size)}
-      >
-        <Geschaefte />
-        <FilterFields />
-      </SplitPane>
-    )
-  }
+FilterFieldsLayout.propTypes = {
+  config: PropTypes.object.isRequired,
+  configSetKey: PropTypes.func.isRequired,
 }
 
 export default FilterFieldsLayout
