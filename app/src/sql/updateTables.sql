e9845c9edf32e85486fe0b1646ab3aaa7de6870d
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-25 12:03:37 +0100

    update from importing

--- app/src/sql/updateTables.sql
+++ app/src/sql/updateTables.sql
@@ -16,13 +16,16 @@ alter table interne2 rename to interne
 
 
 -- geschaefte
-INSERT INTO geschaefte2 (abteilung, aktennummer, aktenstandort, ausloeser, benutzer, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidBvvJahr, entscheidBvvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, geschaeftsart, idGeschaeft, idKontaktExtern_readonly, idKontaktIntern_readonly, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, rechtsmittelInstanz, rechtsmittelErledigung, rechtsmittelEntscheidNr, rechtsmittelEntscheidDatum, status, verantwortlich, vermerk, vermerkIntern, zustaendigeDirektion)
-SELECT abteilung, aktennummer, aktenstandort, ausloeser, benutzer, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidBvvJahr, entscheidBvvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, geschaeftsart, idGeschaeft, idKontaktExtern_readonly, idKontaktIntern_readonly, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, rechtsmittelInstanz, rechtsmittelErledigung, rechtsmittelEntscheidNr, rechtsmittelEntscheidDatum, status, verantwortlich, vermerk, vermerkIntern, zustaendigeDirektion
+INSERT INTO geschaefte2 (abteilung, aktennummer, aktenstandort, ausloeser, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidBvvJahr, entscheidBvvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, geschaeftsart, idGeschaeft, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, parlVorstossZustaendigkeitAwel, rechtsmittelInstanz, rechtsmittelErledigung, rechtsmittelEntscheidNr, rechtsmittelEntscheidDatum, rechtsmittelTxt, status, verantwortlich, vermerk, vermerkIntern, zustaendigeDirektion)
+SELECT abteilung, aktennummer, aktenstandort, ausloeser, datumAusgangAwel, datumEingangAwel, details, entscheidAwelJahr, entscheidAwelNr, entscheidBdvJahr, entscheidBdvNr, entscheidBvvJahr, entscheidBvvNr, entscheidKrJahr, entscheidKrNr, entscheidRrbJahr, entscheidRrbNr, fristAbteilung, fristAmtschef, fristAwel, fristDirektion, fristMitarbeiter, gegenstand, geschaeftsart, idGeschaeft, idVorgeschaeft, mutationsdatum, mutationsperson, naechsterSchritt, ort, parlVorstossStufe, parlVorstossTyp, parlVorstossZustaendigkeitAwel, rechtsmittelInstanz, rechtsmittelErledigung, rechtsmittelEntscheidNr, rechtsmittelEntscheidDatum, rechtsmittelTxt, status, verantwortlich, vermerk, vermerkIntern, zustaendigeDirektion
 FROM geschaefte;
 
+-- WARNING:
+-- STOP CASCADING ON DELETE REFERENCES BEFORE DROPPING GESCHAEFTE
+-- OTHERWISE SOME TABLES ARE EMPTIED, FOR INSCTANCE geschaefteKontakteIntern
 DROP TABLE geschaefte;
-
 ALTER TABLE geschaefte2 RENAME TO geschaefte;
+-- enable references
 
 
 --geschaefteKontakteIntern
