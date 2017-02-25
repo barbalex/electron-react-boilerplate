cd06c5a820d75b4439ae76982c9d9269bec1056e
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-25 22:33:30 +0100

    Solve crashes when calculating history

--- app/src/getHistoryOfGeschaeft.js
+++ app/src/getHistoryOfGeschaeft.js
@@ -4,14 +4,23 @@ import getIdNachgeschaeft from './getIdNachgeschaeft'
 export default function (geschaefte, activeId) {
   const history = [activeId]
   let idVorgeschaeft = getIdVorgeschaeft(geschaefte, activeId)
+  if (idVorgeschaeft) history.unshift(idVorgeschaeft)
+
   while (idVorgeschaeft) {
-    history.unshift(idVorgeschaeft)
     idVorgeschaeft = getIdVorgeschaeft(geschaefte, idVorgeschaeft)
+    // need to prevent endless loop when two geschaefte set each other as vorgeschaeft
+    if (idVorgeschaeft && !history.includes(idVorgeschaeft)) {
+      history.unshift(idVorgeschaeft)
+    }
+    if (history.includes(idVorgeschaeft)) {
+      idVorgeschaeft = null
+    }
   }
   let idNachgeschaeft = getIdNachgeschaeft(geschaefte, activeId)
+  if (idNachgeschaeft) history.push(idNachgeschaeft)
   while (idNachgeschaeft) {
-    history.push(idNachgeschaeft)
     idNachgeschaeft = getIdNachgeschaeft(geschaefte, idNachgeschaeft)
+    if (idNachgeschaeft) history.push(idNachgeschaeft)
   }
   return history
 }
