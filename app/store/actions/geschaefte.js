c683ba9d4fc2c7f280319d9895f7cdd00dde3b3a
Alexander Gabriel <alex@gabriel-software.ch>
2017-02-27 23:37:12 +0100

    work on store

--- app/store/actions/geschaefte.js
+++ app/store/actions/geschaefte.js
@@ -2,6 +2,7 @@
 import { action } from 'mobx'
 import { browserHistory } from 'react-router'
 import _ from 'lodash'
+import moment from 'moment'
 
 import getGeschaefteFromDb from '../../src/getGeschaefteFromDb'
 import getGekoFromDb from '../../src/getGekoFromDb'
@@ -12,7 +13,6 @@ import getInterneOptions from '../../src/getInterneOptions'
 import getExterneOptions from '../../src/getExterneOptions'
 import updateGeschaeft from '../../src/updateGeschaeft'
 import updateGeko from '../../src/updateGeko'
-import updateLink from '../../src/updateLink'
 import filterGeschaefte from '../../src/filterGeschaefte'
 import sortIdsBySortFields from '../../src/sortIdsBySortFields'
 import newGeschaeftInDb from '../../src/newGeschaeftInDb'
@@ -28,15 +28,6 @@ export default (store) => ({
   geschaeftPdfShow: action(() =>
     browserHistory.push('/geschaeftPdf')
   ),
-  geschaefteGet: action(() => {
-    store.geschaefte.fetching = true
-    store.geschaefte.error = []
-  }),
-  geschaefteGetSuccess: action((geschaefte) => {
-    store.geschaefte.fetching = false
-    store.geschaefte.error = []
-    store.geschaefte.geschaefte = geschaefte
-  }),
   geschaefteSetGefilterteIds: action(() => {
     const { geschaefte } = store
     const { filterFields, filterFulltext, sortFields } = geschaefte
@@ -49,21 +40,23 @@ export default (store) => ({
     geschaefteGefilterteIds = sortIdsBySortFields(geschaefte, geschaefteGefilterteIds, sortFields)
     store.geschaefte.geschaefteGefilterteIds = geschaefteGefilterteIds
   }),
-  geschaefteGetError: action((error) => {
-    store.geschaefte.fetching = false
-    store.geschaefte.error.push(error)
-  }),
   getGeschaefte: action(() => {
     const { app, routing } = store
-    store.geschaefteGet()
+    store.geschaefte.fetching = true
+    store.geschaefte.error = []
     getGeschaefteFromDb(app.db)
       .then((geschaefte) => {
-        store.geschaefteGetSuccess(geschaefte)
+        store.geschaefte.fetching = false
+        store.geschaefte.error = []
+        store.geschaefte.geschaefte = geschaefte
         if (routing.locationBeforeTransitions.pathname !== '/geschaefte') {
           browserHistory.push('/geschaefte')
         }
       })
-      .catch(error => store.geschaefteGetError(error))
+      .catch(error => {
+        store.geschaefte.fetching = false
+        store.geschaefte.error.push(error)
+      })
   }),
   geschaefteFilterByFields: action((filterFields, filterType = 'nach Feldern') => {
     const { routing, pages } = store
@@ -138,7 +131,7 @@ export default (store) => ({
     geschaefteGefilterteIds = sortIdsBySortFields(geschaefte, geschaefteGefilterteIds, sortFields)
     store.geschaefte.geschaefteGefilterteIds = geschaefteGefilterteIds
     store.geschaefte.filterType = filterType || null
-    store.geschaefte.filterFulltext = action.filterFulltext
+    store.geschaefte.filterFulltext = filterFulltext
     store.geschaefte.filterFields = []
     store.geschaefte.activeId = null
     /**
@@ -171,72 +164,258 @@ export default (store) => ({
   }),
   getGeko: action(() => {
     const { app } = store
-    store.gekoGet()
-    getGekoFromDb(app.db)
-      .then((geko) => {
-        store.gekoGetSuccess(geko)
-      })
-      .catch(error => store.gekoGetError(error))
-  }),
-  gekoGet: action(() => {
     store.geschaefte.fetching = true
     store.geschaefte.error = []
+    getGekoFromDb(app.db)
+      .then((geko) => {
+        store.geschaefte.fetching = false
+        store.geschaefte.error = []
+        store.geschaefte.geko = geko
+      })
+      .catch(error => {
+        store.geschaefte.fetching = false
+        store.geschaefte.error.push(error)
+      })
   }),
-  gekoGetSuccess: action((geko) => {
-    store.geschaefte.fetching = false
+  getLinks: action(() => {
+    const { app } = store
+    store.geschaefte.fetching = true
     store.geschaefte.error = []
-    store.geschaefte.geko = geko
+    getLinkFromDb(app.db)
+      .then((links) => {
+        store.geschaefte.fetching = false
+        store.geschaefte.error = []
+        store.geschaefte.links = links
+      })
+      .catch(error => {
+        store.geschaefte.fetching = false
+        store.geschaefte.error.push(error)
+      })
   }),
-  gekoGetError: action((error) => {
-    store.geschaefte.fetching = false
-    store.geschaefte.error.push(error)
+  /*
+   * GESCHAEFT
+   */
+  geschaeftNewCreate: action(() => {
+    const { app, user, routing } = store
+    newGeschaeftInDb(app.db, user.username)
+      .then((geschaeft) => {
+        store.geschaefte.geschaefte.unshift(geschaeft)
+          /**
+           * need to add new id to geschaefteGefilterteIds
+           */
+        store.geschaefte.geschaefteGefilterteIds.unshift(geschaeft.idGeschaeft)
+        store.geschaeftToggleActivated(geschaeft.idGeschaeft)
+        if (routing.locationBeforeTransitions.pathname !== '/geschaefte') {
+          browserHistory.push('/geschaefte')
+        }
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
   }),
-  xxx: action(() => {
-
+  geschaeftRemove: action((idGeschaeft) => {
+    const { app, geschaefteKontakteIntern, geschaefteKontakteExtern, geschaefte } = store
+    deleteGeschaeft(app.db, idGeschaeft)
+      .then(() => {
+        store.geschaeftRemoveDeleteIntended(idGeschaeft)
+        store.geschaefte.geschaefte = store.geschaefte.geschaefte.filter(g =>
+          g.idGeschaeft !== idGeschaeft
+        )
+        // need to delete geschaefteKontakteIntern in store
+        const geschaefteKontakteInternToDelete = geschaefteKontakteIntern.geschaefteKontakteIntern
+          .filter(g => g.idGeschaeft === idGeschaeft)
+        geschaefteKontakteInternToDelete.forEach(g =>
+          store.geschaeftKontaktInternDelete(idGeschaeft, g.idKontakt)
+        )
+        // need to delete geschaefteKontakteExtern in store
+        const geschaefteKontakteExternToDelete = geschaefteKontakteExtern.geschaefteKontakteExtern
+          .filter(g => g.idGeschaeft === idGeschaeft)
+        geschaefteKontakteExternToDelete.forEach(g =>
+          store.geschaefteKontakteExternActions.geschaeftKontaktExternDelete(idGeschaeft, g.idKontakt)
+        )
+        // need to delete geKo in store
+        const gekoToRemove = geschaefte.geko.filter(g => g.idGeschaeft === idGeschaeft)
+        gekoToRemove.forEach(g =>
+          store.gekoRemove(idGeschaeft, g.gekoNr)
+        )
+        // need to delete links in store
+        const linksToRemove = geschaefte.links.filter(l => l.idGeschaeft === idGeschaeft)
+        linksToRemove.forEach(l =>
+          store.linkDelete(idGeschaeft, l.url)
+        )
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
   }),
-  xxx: action(() => {
-
+  geschaeftRemoveDeleteIntended: action(() => {
+    store.geschaefte.willDelete = false
   }),
-  xxx: action(() => {
-
+  geschaeftSetDeleteIntended: action(() => {
+    store.geschaefte.willDelete = true
   }),
-  xxx: action(() => {
-
+  geschaefteChangeState: action((idGeschaeft, field, value) => {
+    const { user } = store
+    const { geschaefte } = store.geschaefte
+    const username = user.username
+    const geschaeft = geschaefte.find(g => g.idGeschaeft === idGeschaeft)
+    if (geschaeft) {
+      geschaeft[field] = value
+      geschaeft.mutationsperson = username
+      geschaeft.mutationsdatum = moment().format('YYYY-MM-DD HH:mm:ss')
+    } else {
+      store.geschaefte.error.push(new Error('Das Geschäft wurde nicht aktualisiert'))
+    }
   }),
-  xxx: action(() => {
-
+  changeGeschaeftInDb: action((idGeschaeft, field, value) =>
+    // no need to do something on then
+    // ui was updated on GESCHAEFTE_CHANGE_STATE
+    updateGeschaeft(store.app.db, idGeschaeft, field, value, store.user.username)
+      .catch((error) =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  rechtsmittelErledigungOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'rechtsmittelErledigung')
+      .then(rechtsmittelErledigungOptions => {
+        store.geschaefte.rechtsmittelErledigungOptions = rechtsmittelErledigungOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  parlVorstossTypOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'parlVorstossTyp')
+      .then(parlVorstossTypOptions => {
+        store.geschaefte.parlVorstossTypOptions = parlVorstossTypOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  statusOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'status')
+      .then(statusOptions => {
+        store.geschaefte.statusOptions = statusOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  faelligeStatiOptionsGet: action(() => {
+    getFaelligeStatiOptions(store.app.db)
+      .then(faelligeStatiOptions => {
+        store.geschaefte.faelligeStatiOptions = faelligeStatiOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
   }),
-  xxx: action(() => {
-
+  geschaeftsartOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'geschaeftsart')
+      .then(geschaeftsartOptions => {
+        store.geschaefte.geschaeftsartOptions = geschaeftsartOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  aktenstandortOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'aktenstandort')
+      .then(aktenstandortOptions => {
+        store.geschaefte.aktenstandortOptions = aktenstandortOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  interneOptionsGet: action(() =>
+    getInterneOptions(store.app.db)
+      .then(interneOptions => {
+        store.geschaefte.interneOptions = interneOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  externeOptionsGet: action(() =>
+    getExterneOptions(store.app.db)
+      .then(externeOptions => {
+        store.geschaefte.externeOptions = externeOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  rechtsmittelInstanzOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'rechtsmittelInstanz')
+      .then(rechtsmittelInstanzOptions => {
+        store.geschaefte.rechtsmittelInstanzOptions = rechtsmittelInstanzOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  abteilungOptionsGet: action(() =>
+    getDropdownOptions(store.app.db, 'abteilung')
+      .then(abteilungOptions => {
+        store.geschaefte.abteilungOptions = abteilungOptions
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  gekoNewCreate: action((idGeschaeft, gekoNr) =>
+    newGekoInDb(store.app.db, idGeschaeft, gekoNr)
+      .then((geko) =>
+        store.geschaefte.geko.unshift(geko)
+      )
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  gekoRemove: action((idGeschaeft, gekoNr) =>
+    deleteGeko(store.app.db, idGeschaeft, gekoNr)
+      .then(() => {
+        store.geschaefte.geko = store.geschaefte.geko.filter(g => (
+          (g.idGeschaeft !== idGeschaeft) ||
+          (g.gekoNr !== gekoNr)
+        ))
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  changeGekoInDb: action((idGeschaeft, gekoNr, field, value) =>
+    // no need to do something on then
+    // ui was updated on GEKO_CHANGE_STATE
+    updateGeko(store.app.db, idGeschaeft, gekoNr, field, value)
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  linkNewCreate: action((idGeschaeft, url) =>
+    newLinkInDb(store.app.db, idGeschaeft, url)
+      .then(() =>
+        store.geschaefte.links.unshift({ idGeschaeft, url })
+      )
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
+  ),
+  linkRemove: action((idGeschaeft, url) => {
+    deleteLink(store.app.db, idGeschaeft, url)
+      .then(() => {
+        store.linkDelete(idGeschaeft, url)
+      })
+      .catch(error =>
+        store.geschaefte.error.push(error)
+      )
   }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
-  }),
-  xxx: action(() => {
-
+  linkDelete: action((idGeschaeft, url) => {
+    store.geschaefte.links = store.geschaefte.links.filter(l => (
+      (l.idGeschaeft !== idGeschaeft) ||
+      (l.url !== url)
+    ))
   }),
 })
