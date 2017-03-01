35d440c2ed07fa1bef84d585379e3abef72aee24
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-01 20:50:37 +0100

    Optimize

--- app/src/exportGeschaefte.js
+++ app/src/exportGeschaefte.js
@@ -9,25 +9,25 @@ const { dialog } = remote
 
 function getDataArrayFromExportObjects(exportObjects) {
   const dataArray = []
-  console.log('exportGeschaefte: exportObjects:', exportObjects)
   // first the field names:
-  dataArray.push(Object.keys(exportObjects[0]))
+  dataArray.push(
+    Object.keys(exportObjects[0])
+  )
   // then the field values
   exportObjects.forEach(object =>
-    dataArray
-      .push(
-        Object.keys(object)
-          .map((key, index) => {
-            /**
-             * exceljs errors out if first member of array is null
-             * see: https://github.com/guyonroche/exceljs/issues/111
-             */
-            if (object[key] === null && index === 0) {
-              return ''
-            }
-            return object[key]
-          })
-      ))
+    dataArray.push(
+      Object.keys(object)
+        .map((key, index) => {
+          /**
+           * exceljs errors out if first member of array is null
+           * see: https://github.com/guyonroche/exceljs/issues/111
+           */
+          if (object[key] === null && index === 0) {
+            return ''
+          }
+          return object[key]
+        })
+    ))
   return dataArray
 }
 
