ea4dede50f4283b1cad033f118b8c0036c33f8cd
Alexander Gabriel <alex@gabriel-software.ch>
2017-03-02 14:41:22 +0100

    modularize

---
+++ app/src/addComputedValuesToGeschaefte.js
@@ -0,0 +1,36 @@
+/* eslint-disable no-param-reassign */
+import getDauerBisFristMitarbeiter from './getDauerBisFristMitarbeiter'
+import getFristMitarbeiterWarnung from './getFristMitarbeiterWarnung'
+import getItKontoForVerantwortlich from './getItKontoForVerantwortlich'
+
+export default (store) =>
+  store.geschaefte.geschaefte.map((g) => {
+    const { interneOptions, externeOptions, geko, links, faelligeStatiOptions } = store.geschaefte
+    const interne = store.geschaefteKontakteIntern.geschaefteKontakteIntern
+    const externe = store.geschaefteKontakteExtern.geschaefteKontakteExtern
+    g.verantwortlichName = interneOptions
+      .filter(i => i.kurzzeichen === g.verantwortlich)
+      .map(i =>
+        (i.name ? `${i.vorname} ${i.name}` : '')
+      )
+      .join(', ')
+    g.interne = interne
+      .filter(i => i.idGeschaeft === g.idGeschaeft)
+      .map((gk) =>
+        interneOptions.find(i => i.id === gk.idKontakt) || null
+      )
+    g.externe = externe
+      .filter(i => i.idGeschaeft === g.idGeschaeft)
+      .map((gk) =>
+        externeOptions.find(i => i.id === gk.idKontakt) || null
+      )
+    g.verantwortlichItKonto = getItKontoForVerantwortlich(store.geschaefte.interneOptions, g.verantwortlich)
+    g.geko = geko
+      .filter(gko => gko.idGeschaeft === g.idGeschaeft)
+    g.links = links
+      .filter(link => link.idGeschaeft === g.idGeschaeft)
+    g.kannFaelligSein = faelligeStatiOptions ? faelligeStatiOptions.includes(g.status) : false
+    g.dauerBisFristMitarbeiter = getDauerBisFristMitarbeiter(g)
+    g.fristMitarbeiterWarnung = getFristMitarbeiterWarnung(g.dauerBisFristMitarbeiter)
+    return g
+  })
--- app/store/index.js
+++ app/store/index.js
@@ -1,9 +1,6 @@
-/* eslint-disable no-param-reassign */
 import { extendObservable, computed } from 'mobx'
-import _ from 'lodash'
-import moment from 'moment'
 
-import observablehistory from './observablehistory'
+import observablehistory from './observableHistory'
 import app from './app'
 import appActions from './actions/app'
 import user from './user'
@@ -17,12 +14,9 @@ import geschaefteKontakteInternActions from './actions/geschaefteKontakteIntern'
 import geschaefteKontakteExtern from './geschaefteKontakteExtern'
 import geschaefteKontakteExternActions from './actions/geschaefteKontakteExtern'
 import geschaefteActions from './actions/geschaefte'
-import getDauerBisFristMitarbeiter from '../src/getDauerBisFristMitarbeiter'
-import getFristMitarbeiterWarnung from '../src/getFristMitarbeiterWarnung'
-import getItKontoForVerantwortlich from '../src/getItKontoForVerantwortlich'
-import filterGeschaefteByFulltext from '../src/filterGeschaefteByFulltext'
-import filterGeschaefteByFilterFields from '../src/filterGeschaefteByFilterFields'
-import isDateField from '../src/isDateField'
+import addComputedValuesToGeschaefte from '../src/addComputedValuesToGeschaefte'
+import filterGeschaeftePlus from '../src/filterGeschaeftePlus'
+import sortGeschaeftePlusFiltered from '../src/sortGeschaeftePlusFiltered'
 
 function Store() {
   this.history = observablehistory
@@ -34,73 +28,14 @@ function Store() {
     error: [],
     geschaefte: [],
     geschaeftePlus: computed(() =>
-      this.geschaefte.geschaefte.map((g) => {
-        const { interneOptions, externeOptions, geko, links, faelligeStatiOptions } = this.geschaefte
-        const interne = this.geschaefteKontakteIntern.geschaefteKontakteIntern
-        const externe = this.geschaefteKontakteExtern.geschaefteKontakteExtern
-        g.verantwortlichName = interneOptions
-          .filter(i => i.kurzzeichen === g.verantwortlich)
-          .map(i =>
-            (i.name ? `${i.vorname} ${i.name}` : '')
-          )
-          .join(', ')
-        g.interne = interne
-          .filter(i => i.idGeschaeft === g.idGeschaeft)
-          .map((gk) =>
-            interneOptions.find(i => i.id === gk.idKontakt) || null
-          )
-        g.externe = externe
-          .filter(i => i.idGeschaeft === g.idGeschaeft)
-          .map((gk) =>
-            externeOptions.find(i => i.id === gk.idKontakt) || null
-          )
-        g.verantwortlichItKonto = getItKontoForVerantwortlich(this.geschaefte.interneOptions, g.verantwortlich)
-        g.geko = geko
-          .filter(gko => gko.idGeschaeft === g.idGeschaeft)
-        g.links = links
-          .filter(link => link.idGeschaeft === g.idGeschaeft)
-        g.kannFaelligSein = faelligeStatiOptions ? faelligeStatiOptions.includes(g.status) : false
-        g.dauerBisFristMitarbeiter = getDauerBisFristMitarbeiter(g)
-        g.fristMitarbeiterWarnung = getFristMitarbeiterWarnung(g.dauerBisFristMitarbeiter)
-        return g
-      })
+      addComputedValuesToGeschaefte(this)
     ),
-    geschaeftePlusFiltered: computed(() => {
-      const { filterFulltext, filterFields, geschaeftePlus, geschaefte } = this.geschaefte
-      const existsFilterFulltext = !!filterFulltext
-      const existsFilterFields = filterFields.length > 0
-
-      if (existsFilterFulltext) {
-        return filterGeschaefteByFulltext(geschaeftePlus, filterFulltext)
-      } else if (existsFilterFields) {
-        return filterGeschaefteByFilterFields(geschaeftePlus, filterFields)
-      }
-      return geschaefte
-    }),
-    geschaeftePlusFilteredAndSorted: computed(() => {
-      const geschaeftePassed = this.geschaefte.geschaeftePlusFiltered
-      const { sortFields } = this.geschaefte
-      let geschaefte
-      sortFields.forEach((sf) => {
-        geschaefte = _.sortBy(geschaeftePassed, (g) => {
-          if (g[sf.field]) {
-            if (isDateField(sf.field)) {
-              // need to reformat date
-              return moment(g[sf.field], 'DD.MM.YYYY').format('YYYY-MM-DD')
-            }
-            return g[sf.field]
-          }
-          return 'ZZZZ'
-        })
-        if (sf.direction === 'DESCENDING') {
-          geschaefte = geschaefte.reverse()
-        }
-      })
-      if (sortFields.length === 0) {
-        geschaefte = _.sortBy(geschaeftePassed, 'idGeschaeft').reverse()
-      }
-      return geschaefte
-    }),
+    geschaeftePlusFiltered: computed(() =>
+      filterGeschaeftePlus(this)
+    ),
+    geschaeftePlusFilteredAndSorted: computed(() =>
+      sortGeschaeftePlusFiltered(this)
+    ),
     links: [],
     geko: [],
     filterFields: [],
