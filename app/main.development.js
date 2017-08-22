0d75c4b2653151a92757a4576d68b6ffb2222f41
Alexander Gabriel <alex@gabriel-software.ch>
2017-08-22 20:56:34 +0200

    deactivate experimental features

--- app/main.development.js
+++ app/main.development.js
@@ -23,24 +23,20 @@ app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
 
-
 const installExtensions = async () => {
   if (process.env.NODE_ENV === 'development') {
     const installer = require('electron-devtools-installer') // eslint-disable-line global-require
 
-    const extensions = [
-      'REACT_DEVELOPER_TOOLS',
-      'REDUX_DEVTOOLS'
-    ]
+    const extensions = ['REACT_DEVELOPER_TOOLS', 'REDUX_DEVTOOLS']
 
     const forceDownload = !!process.env.UPGRADE_EXTENSIONS
 
     // TODO: Use async interation statement.
     //       Waiting on https://github.com/tc39/proposal-async-iteration
     //       Promises will fail silently, which isn't what we want in development
-    return Promise
-      .all(extensions.map(name => installer.default(installer[name], forceDownload)))
-      .catch(console.log)
+    return Promise.all(
+      extensions.map(name => installer.default(installer[name], forceDownload))
+    ).catch(console.log)
   }
 }
 
@@ -48,9 +44,10 @@ const browserWindowOptions = {
   width: 1800,
   height: 1024,
   icon: './app/etc/app.png',
+  /*
   webPreferences: {
     experimentalFeatures: true
-  },
+  },*/
   show: false,
 }
 
@@ -59,7 +56,9 @@ const browserWindowOptions = {
 const lastWindowState = getConfigSync().lastWindowState
 if (lastWindowState) {
   if (lastWindowState.width) browserWindowOptions.width = lastWindowState.width
-  if (lastWindowState.height) browserWindowOptions.height = lastWindowState.height
+  if (lastWindowState.height) {
+    browserWindowOptions.height = lastWindowState.height
+  }
   if (lastWindowState.x) browserWindowOptions.x = lastWindowState.x
   if (lastWindowState.y) browserWindowOptions.y = lastWindowState.y
 }
@@ -91,7 +90,7 @@ app.on('ready', async () => {
       y: bounds.y,
       width: bounds.width,
       height: bounds.height,
-      maximized: mainWindow.isMaximized()
+      maximized: mainWindow.isMaximized(),
     })
   })
 
@@ -100,144 +99,191 @@ app.on('ready', async () => {
     mainWindow.webContents.on('context-menu', (e, props) => {
       const { x, y } = props
 
-      Menu.buildFromTemplate([{
-        label: 'Inspect element',
-        click() {
-          mainWindow.inspectElement(x, y)
-        }
-      }]).popup(mainWindow)
+      Menu.buildFromTemplate([
+        {
+          label: 'Inspect element',
+          click() {
+            mainWindow.inspectElement(x, y)
+          },
+        },
+      ]).popup(mainWindow)
     })
   }
 
   if (process.platform === 'darwin') {
-    template = [{
-      label: 'Electron',
-      submenu: [{
-        label: 'About ElectronReact',
-        selector: 'orderFrontStandardAboutPanel:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Services',
-        submenu: []
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Hide ElectronReact',
-        accelerator: 'Command+H',
-        selector: 'hide:'
-      }, {
-        label: 'Hide Others',
-        accelerator: 'Command+Shift+H',
-        selector: 'hideOtherApplications:'
-      }, {
-        label: 'Show All',
-        selector: 'unhideAllApplications:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Quit',
-        accelerator: 'Command+Q',
-        click() {
-          app.quit()
-        }
-      }]
-    }, {
-      label: 'Edit',
-      submenu: [{
-        label: 'Undo',
-        accelerator: 'Command+Z',
-        selector: 'undo:'
-      }, {
-        label: 'Redo',
-        accelerator: 'Shift+Command+Z',
-        selector: 'redo:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Cut',
-        accelerator: 'Command+X',
-        selector: 'cut:'
-      }, {
-        label: 'Copy',
-        accelerator: 'Command+C',
-        selector: 'copy:'
-      }, {
-        label: 'Paste',
-        accelerator: 'Command+V',
-        selector: 'paste:'
-      }, {
-        label: 'Select All',
-        accelerator: 'Command+A',
-        selector: 'selectAll:'
-      }]
-    }, {
-      label: 'View',
-      submenu: (process.env.NODE_ENV === 'development') ? [{
-        label: 'Reload',
-        accelerator: 'Command+R',
-        click() {
-          mainWindow.webContents.reload()
-        }
-      }, {
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }, {
-        label: 'Toggle Developer Tools',
-        accelerator: 'Alt+Command+I',
-        click() {
-          mainWindow.toggleDevTools()
-        }
-      }] : [{
-        label: 'Toggle Full Screen',
-        accelerator: 'Ctrl+Command+F',
-        click() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen())
-        }
-      }]
-    }, {
-      label: 'Window',
-      submenu: [{
-        label: 'Minimize',
-        accelerator: 'Command+M',
-        selector: 'performMiniaturize:'
-      }, {
-        label: 'Close',
-        accelerator: 'Command+W',
-        selector: 'performClose:'
-      }, {
-        type: 'separator'
-      }, {
-        label: 'Bring All to Front',
-        selector: 'arrangeInFront:'
-      }]
-    }, {
-      label: 'Help',
-      submenu: [{
-        label: 'Learn More',
-        click() {
-          shell.openExternal('http://electron.atom.io')
-        }
-      }, {
-        label: 'Documentation',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/tree/master/docs#readme')
-        }
-      }, {
-        label: 'Community Discussions',
-        click() {
-          shell.openExternal('https://discuss.atom.io/c/electron')
-        }
-      }, {
-        label: 'Search Issues',
-        click() {
-          shell.openExternal('https://github.com/atom/electron/issues')
-        }
-      }]
-    }]
+    template = [
+      {
+        label: 'Electron',
+        submenu: [
+          {
+            label: 'About ElectronReact',
+            selector: 'orderFrontStandardAboutPanel:',
+          },
+          {
+            type: 'separator',
+          },
+          {
+            label: 'Services',
+            submenu: [],
+          },
+          {
+            type: 'separator',
+          },
+          {
+            label: 'Hide ElectronReact',
+            accelerator: 'Command+H',
+            selector: 'hide:',
+          },
+          {
+            label: 'Hide Others',
+            accelerator: 'Command+Shift+H',
+            selector: 'hideOtherApplications:',
+          },
+          {
+            label: 'Show All',
+            selector: 'unhideAllApplications:',
+          },
+          {
+            type: 'separator',
+          },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click() {
+              app.quit()
+            },
+          },
+        ],
+      },
+      {
+        label: 'Edit',
+        submenu: [
+          {
+            label: 'Undo',
+            accelerator: 'Command+Z',
+            selector: 'undo:',
+          },
+          {
+            label: 'Redo',
+            accelerator: 'Shift+Command+Z',
+            selector: 'redo:',
+          },
+          {
+            type: 'separator',
+          },
+          {
+            label: 'Cut',
+            accelerator: 'Command+X',
+            selector: 'cut:',
+          },
+          {
+            label: 'Copy',
+            accelerator: 'Command+C',
+            selector: 'copy:',
+          },
+          {
+            label: 'Paste',
+            accelerator: 'Command+V',
+            selector: 'paste:',
+          },
+          {
+            label: 'Select All',
+            accelerator: 'Command+A',
+            selector: 'selectAll:',
+          },
+        ],
+      },
+      {
+        label: 'View',
+        submenu:
+          process.env.NODE_ENV === 'development'
+            ? [
+                {
+                  label: 'Reload',
+                  accelerator: 'Command+R',
+                  click() {
+                    mainWindow.webContents.reload()
+                  },
+                },
+                {
+                  label: 'Toggle Full Screen',
+                  accelerator: 'Ctrl+Command+F',
+                  click() {
+                    mainWindow.setFullScreen(!mainWindow.isFullScreen())
+                  },
+                },
+                {
+                  label: 'Toggle Developer Tools',
+                  accelerator: 'Alt+Command+I',
+                  click() {
+                    mainWindow.toggleDevTools()
+                  },
+                },
+              ]
+            : [
+                {
+                  label: 'Toggle Full Screen',
+                  accelerator: 'Ctrl+Command+F',
+                  click() {
+                    mainWindow.setFullScreen(!mainWindow.isFullScreen())
+                  },
+                },
+              ],
+      },
+      {
+        label: 'Window',
+        submenu: [
+          {
+            label: 'Minimize',
+            accelerator: 'Command+M',
+            selector: 'performMiniaturize:',
+          },
+          {
+            label: 'Close',
+            accelerator: 'Command+W',
+            selector: 'performClose:',
+          },
+          {
+            type: 'separator',
+          },
+          {
+            label: 'Bring All to Front',
+            selector: 'arrangeInFront:',
+          },
+        ],
+      },
+      {
+        label: 'Help',
+        submenu: [
+          {
+            label: 'Learn More',
+            click() {
+              shell.openExternal('http://electron.atom.io')
+            },
+          },
+          {
+            label: 'Documentation',
+            click() {
+              shell.openExternal(
+                'https://github.com/atom/electron/tree/master/docs#readme'
+              )
+            },
+          },
+          {
+            label: 'Community Discussions',
+            click() {
+              shell.openExternal('https://discuss.atom.io/c/electron')
+            },
+          },
+          {
+            label: 'Search Issues',
+            click() {
+              shell.openExternal('https://github.com/atom/electron/issues')
+            },
+          },
+        ],
+      },
+    ]
 
     menu = Menu.buildFromTemplate(template)
     Menu.setApplicationMenu(menu)
