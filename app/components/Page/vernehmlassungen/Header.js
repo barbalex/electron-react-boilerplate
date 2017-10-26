66c883093513c051813804e1dc0f55c55ddd45cc
Alexander Gabriel <alex.barbalex@gmail.com>
2017-10-26 18:25:11 +0200

    use styled

--- app/components/Page/vernehmlassungen/Header.js
+++ app/components/Page/vernehmlassungen/Header.js
@@ -3,45 +3,48 @@
  */
 
 import React from 'react'
+import styled from 'styled-components'
+
 import styles from './vernehmlassungen.css'
 
-const PageFristenHeader = () =>
-  <div className={styles.tableHeader}>
-    <div className={styles.tableHeaderRow}>
-      <div
-        className={[
-          styles.columnIdGeschaeft,
-          styles.tableHeaderCell,
-        ].join(' ')}
-      >
-        <b>ID</b><br />Geko Nr.
+const Container = styled.div`border-bottom: 2px solid #717171;`
+const StyledHeaderRow = styled.div`
+  display: flex;
+  padding: 3px;
+`
+const StyledHeaderCell = styled.div`
+  flex: 1;
+  padding: 2px;
+`
+const StyledHeaderId = styled.div`
+  flex: 1;
+  padding: 2px;
+  min-width: 65px;
+  max-width: 65px;
+`
+
+const PageFristenHeader = () => (
+  <Container>
+    <StyledHeaderRow>
+      <StyledHeaderId>
+        <b>ID</b>
+        <br />Geko Nr.
+      </StyledHeaderId>
+      <div className={[styles.columnGegenstand, styles.tableHeaderCell].join(' ')}>
+        <b>Gegenstand</b>
+        <br />Auslöser / Details / Vermerk / nächster Schritt
       </div>
-      <div
-        className={[
-          styles.columnGegenstand,
-          styles.tableHeaderCell,
-        ].join(' ')}
-      >
-        <b>Gegenstand</b><br />Auslöser / Details / Vermerk / nächster Schritt
+      <div className={[styles.columnStatus, styles.tableHeaderCell].join(' ')}>
+        <b>Status</b>
+        <br />Frist
       </div>
-      <div
-        className={[
-          styles.columnStatus,
-          styles.tableHeaderCell,
-        ].join(' ')}
-      >
-        <b>Status</b><br />Frist
+      <div className={[styles.columnKontaktIntern, styles.tableHeaderCell].join(' ')}>
+        <b>Verantwortlich</b>
+        <br />Abteilung
       </div>
-      <div
-        className={[
-          styles.columnKontaktIntern,
-          styles.tableHeaderCell,
-        ].join(' ')}
-      >
-        <b>Verantwortlich</b><br />Abteilung
-      </div>
-    </div>
-  </div>
+    </StyledHeaderRow>
+  </Container>
+)
 
 PageFristenHeader.displayName = 'PageFristenHeader'
 
