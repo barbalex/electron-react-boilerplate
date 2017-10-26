15b0351e6ba679451605325892b81b719d107494
Alexander Gabriel <alex@gabriel-software.ch>
2017-10-26 14:41:16 +0200

    use styled

--- app/components/Pages.js
+++ app/components/Pages.js
@@ -1,27 +1,40 @@
 import React, { PropTypes } from 'react'
 import { observer, inject } from 'mobx-react'
 import compose from 'recompose/compose'
+import styled from 'styled-components'
 
 import Page from './Page'
-import styles from './Pages.css'
 
-const enhance = compose(
-  inject('store'),
-  observer
+const Container = styled.div`
+  background-color: #eee;
+  font-size: 9pt;
+  cursor: default;
+  /*
+  * need defined height and overflow
+  * to make the pages scrollable in UI
+  * is removed in print
+  */
+  overflow-y: auto;
+  height: 100vh;
+
+  @media print {
+    /* remove grey backgrond set for nice UI */
+    background-color: #fff;
+    /* with overflow auto an empty page is inserted between each page */
+    overflow-y: visible !important;
+    /* make sure body grows as needed */
+    height: auto !important;
+
+    page-break-before: avoid !important;
+    page-break-after: avoid !important;
+  }
+`
+
+const enhance = compose(inject('store'), observer)
+
+const Pages = ({ store }) => (
+  <Container>{store.pages.pages.map((page, pageIndex) => <Page key={pageIndex} pageIndex={pageIndex} />)}</Container>
 )
-
-const Pages = ({ store }) =>
-  <div className={styles.body}>
-    {
-      store.pages.pages.map((page, pageIndex) =>
-        <Page
-          key={pageIndex}
-          pageIndex={pageIndex}
-          className={styles.pageContainer}
-        />
-      )
-    }
-  </div>
 
 Pages.displayName = 'Pages'
 
