e661e05b5c0bdc1959694aed7a2543b553a65765
Alexander Gabriel <alex.barbalex@gmail.com>
2017-11-01 17:14:40 +0100

    format

--- app/src/getHistoryOfGeschaefte.js
+++ app/src/getHistoryOfGeschaefte.js
@@ -1,26 +1,16 @@
-/**
- * this works
- * and is used to create history for export
- * BUT:
- * it slows the app far too much if added to reselect
- * even if the function is memoized (which seemed to have no effect)
- */
-
 import _ from 'lodash'
 
 export default function (geschaefte) {
   // build a map
-  const historyMap = new Map(geschaefte.map((g) => {
-    const idArray = (
-      g.idVorgeschaeft ?
-      [g.idVorgeschaeft, g.idGeschaeft] :
-      [g.idGeschaeft]
-    )
-    return [g.idGeschaeft, idArray]
-  }))
-  geschaefte.forEach((g) => {
+  const historyMap = new Map(
+    geschaefte.map(g => {
+      const idArray = g.idVorgeschaeft ? [g.idVorgeschaeft, g.idGeschaeft] : [g.idGeschaeft]
+      return [g.idGeschaeft, idArray]
+    }),
+  )
+  geschaefte.forEach(g => {
     const idArray = historyMap.get(g.idGeschaeft)
-    idArray.forEach((i) => {
+    idArray.forEach(i => {
       const oldValue = historyMap.get(i)
       const newValue = _.union(historyMap.get(i), idArray)
       if (!_.isEqual(oldValue, newValue)) {
