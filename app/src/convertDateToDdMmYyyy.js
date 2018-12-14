814f270fb2554c4965af6f8b638543b8db7edb6a
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:41:03 +0100

    move db

--- app/src/convertDateToDdMmYyyy.js
+++ app/src/convertDateToDdMmYyyy.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 
-export default function convertDateToDdMmYyyy(date) {
+export default date => {
   // make sure not to convert empty values
   if (!date) return ''
   return moment(date, 'YYYY-MM-DD').format('DD.MM.YYYY')
--- app/src/createOptions.js
+++ app/src/createOptions.js
@@ -1,20 +1,12 @@
 import React from 'react'
 
-export default function (values) {
+export default values => {
   if (!values) return []
-  const options = values.map((val, index) =>
-    <option
-      key={index + 1}
-      value={val}
-    >
+  const options = values.map((val, index) => (
+    <option key={index + 1} value={val}>
       {val}
     </option>
-  )
-  options.unshift(
-    <option
-      key={0}
-      value=""
-    />
-  )
+  ))
+  options.unshift(<option key={0} value="" />)
   return options
 }
