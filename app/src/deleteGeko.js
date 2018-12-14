bf55e472b6c647054594496f6ad8ea83f302e10d
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:40:58 +0100

    move db

--- app/src/deleteGeko.js
+++ app/src/deleteGeko.js
@@ -1,10 +1,11 @@
-export default function(db, idGeschaeft, gekoNr) {
-  const sql = `
-      DELETE FROM
-        geko
-      WHERE
-        idGeschaeft = @idGeschaeft AND
-        gekoNr = @gekoNr`
+const sql = `
+  DELETE FROM
+    geko
+  WHERE
+    idGeschaeft = @idGeschaeft AND
+    gekoNr = @gekoNr`
+
+export default (db, idGeschaeft, gekoNr) => {
   try {
     db.prepare(sql).run({ idGeschaeft, gekoNr })
   } catch (error) {
--- app/src/deleteGeschaeftKontaktExtern.js
+++ app/src/deleteGeschaeftKontaktExtern.js
@@ -1,11 +1,11 @@
-export default (db, idGeschaeft, idKontakt) => {
-  const sql = `
-    DELETE FROM
-      geschaefteKontakteExtern
-    WHERE
-      idGeschaeft = @idGeschaeft
-      AND idKontakt = @idKontakt`
+const sql = `
+  DELETE FROM
+    geschaefteKontakteExtern
+  WHERE
+    idGeschaeft = @idGeschaeft
+    AND idKontakt = @idKontakt`
 
+export default (db, idGeschaeft, idKontakt) => {
   try {
     db.prepare(sql).run({ idGeschaeft, idKontakt })
   } catch (error) {
