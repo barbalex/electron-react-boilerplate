41828b2cc3fedbb3a82af07a760298b3f392f06e
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:41:07 +0100

    move db

--- app/src/convertDateToYyyyMmDd.js
+++ app/src/convertDateToYyyyMmDd.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 
-export default function convertDateToYyyyMmDd(date) {
+export default date => {
   // make sure not to convert empty values
   if (!date) return ''
   return moment(date, 'DD.MM.YYYY').format('YYYY-MM-DD')
--- app/src/deleteGeschaeftKontaktIntern.js
+++ app/src/deleteGeschaeftKontaktIntern.js
@@ -1,11 +1,11 @@
-export default function(db, idGeschaeft, idKontakt) {
-  const sql = `
-    DELETE FROM
-      geschaefteKontakteIntern
-    WHERE
-      idGeschaeft = @idGeschaeft
-      AND idKontakt = @idKontakt`
+const sql = `
+  DELETE FROM
+    geschaefteKontakteIntern
+  WHERE
+    idGeschaeft = @idGeschaeft
+    AND idKontakt = @idKontakt`
 
+export default (db, idGeschaeft, idKontakt) => {
   try {
     db.prepare(sql).run({ idGeschaeft, idKontakt })
   } catch (error) {
