a3c5fe4412953560297dea7ce04b4259fa3a8ff3
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:20:49 +0100

    move db

--- app/src/deleteGeko.js
+++ app/src/deleteGeko.js
@@ -1,15 +1,14 @@
-export default function (db, idGeschaeft, gekoNr) {
-  return new Promise((resolve, reject) => {
-    const sql = `
+export default function(db, idGeschaeft, gekoNr) {
+  const sql = `
       DELETE FROM
         geko
       WHERE
-        idGeschaeft = ${idGeschaeft} AND
-        gekoNr = '${gekoNr}'`
-
-    db.run(sql, (error) => {
-      if (error) reject(error)
-      resolve(true)
-    })
-  })
+        idGeschaeft = @idGeschaeft AND
+        gekoNr = @gekoNr`
+  try {
+    db.prepare(sql).run({ idGeschaeft, gekoNr })
+  } catch (error) {
+    throw error
+  }
+  return true
 }
