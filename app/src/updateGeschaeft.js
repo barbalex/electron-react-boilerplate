5a09a6073249fe028fca6cf05b522a0382b94018
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:21:12 +0100

    move db

--- app/src/updateGeschaeft.js
+++ app/src/updateGeschaeft.js
@@ -1,37 +1,33 @@
 import moment from 'moment'
+import app from 'ampersand-app'
+
 import isDateField from './isDateField'
 import convertDateToYyyyMmDd from './convertDateToYyyyMmDd'
 
-export default function (
-  db,
-  idGeschaeft,
-  field,
-  value,
-  username,
-) {
-  return new Promise((resolve, reject) => {
-    /**
-     * if field is date field
-     * convert DD.MM.YYYY to YYYY-MM-DD
-     */
-    let value2 = value
-    if (isDateField(field)) {
-      value2 = convertDateToYyyyMmDd(value)
-    }
-    const now = moment().format('YYYY-MM-DD HH:mm:ss')
-    const sql = `
-      UPDATE
-        geschaefte
-      SET
-        ${field} = '${value2}',
-        mutationsdatum = '${now}',
-        mutationsperson = '${username}'
-      WHERE
-        idGeschaeft = ${idGeschaeft}`
-
-    db.run(sql, (error) => {
-      if (error) reject(error)
-      resolve(true)
-    })
-  })
+export default function(db, idGeschaeft, field, value, username) {
+  const { store } = app
+  /**
+   * if field is date field
+   * convert DD.MM.YYYY to YYYY-MM-DD
+   */
+  let value2 = value
+  if (isDateField(field)) {
+    value2 = convertDateToYyyyMmDd(value)
+  }
+  const now = moment().format('YYYY-MM-DD HH:mm:ss')
+  const sql = `
+    UPDATE
+      geschaefte
+    SET
+      @field = @value2,
+      mutationsdatum = @now,
+      mutationsperson = @username
+    WHERE
+      idGeschaeft = @idGeschaeft`
+  try {
+    db.prepare(sql).run({ field, value2, now, username })
+  } catch (error) {
+    store.geschaefte.error.push(error)
+  }
+  return true
 }
