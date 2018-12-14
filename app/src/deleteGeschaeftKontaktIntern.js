c80b202af725e9d40160181d6c5a9615d38cc756
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-14 18:21:01 +0100

    move db

--- app/src/deleteGeschaeftKontaktIntern.js
+++ app/src/deleteGeschaeftKontaktIntern.js
@@ -1,15 +1,14 @@
-export default function (db, idGeschaeft, idKontakt) {
-  return new Promise((resolve, reject) => {
-    const sql = `
-      DELETE FROM
-        geschaefteKontakteIntern
-      WHERE
-        idGeschaeft = ${idGeschaeft}
-        AND idKontakt = ${idKontakt}`
+export default function(db, idGeschaeft, idKontakt) {
+  const sql = `
+    DELETE FROM
+      geschaefteKontakteIntern
+    WHERE
+      idGeschaeft = @idGeschaeft
+      AND idKontakt = @idKontakt`
 
-    db.run(sql, (error) => {
-      if (error) reject(error)
-      resolve(true)
-    })
-  })
+  try {
+    db.prepare(sql).run({ idGeschaeft, idKontakt })
+  } catch (error) {
+    throw error
+  }
 }
