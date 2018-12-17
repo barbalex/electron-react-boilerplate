1247c03b25a0f6ffec2136bf4d21b30cb577d40f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-17 18:02:33 +0100

    use hooks

--- app/components/FilterFields/AreaZuletztMutiert.js
+++ app/components/FilterFields/AreaZuletztMutiert.js
@@ -1,18 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { FormControl, ControlLabel, InputGroup } from 'react-bootstrap'
 import _ from 'lodash'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
+import { observer } from 'mobx-react-lite'
 import styled from 'styled-components'
 
 import ComparatorSelector from './ComparatorSelector'
 import SortSelector from './SortSelector'
+import storeContext from '../../storeContext'
 
 const interneOptionsList = interneOptions => {
   // sort interneOptions by kurzzeichen
   const interneOptionsWithItKonto = interneOptions.filter(o => !!o.itKonto)
-  const interneOptionsSorted = _.sortBy(interneOptionsWithItKonto, o => o.kurzzeichen.toLowerCase())
+  const interneOptionsSorted = _.sortBy(interneOptionsWithItKonto, o =>
+    o.kurzzeichen.toLowerCase(),
+  )
   const options = interneOptionsSorted.map((o, index) => {
     let times = 5 - o.kurzzeichen.length
     // make sure, times is never < 0
@@ -44,7 +46,8 @@ const interneData = (values, interneOptions) => {
 const Container = styled.div`
   grid-area: areaZuletztMutiert;
   background-color: white;
-  box-shadow: inset 1em 1em 2em rgb(239, 239, 239), inset -1em -1em 2em rgb(239, 239, 239);
+  box-shadow: inset 1em 1em 2em rgb(239, 239, 239),
+    inset -1em -1em 2em rgb(239, 239, 239);
   outline: 1px solid #efefef;
   display: grid;
   grid-template-columns: 175px calc((100% - 10px) - 175px);
@@ -53,45 +56,60 @@ const Container = styled.div`
   padding: 8px;
   align-items: end;
 `
-const FieldVerantwortlichSelector = styled.div`grid-column: 1 / span 1;`
-const FieldVerantwortlichName = styled(FormControl.Static)`grid-column: 2 / span 1;`
+const FieldVerantwortlichSelector = styled.div`
+  grid-column: 1 / span 1;
+`
+const FieldVerantwortlichName = styled(FormControl.Static)`
+  grid-column: 2 / span 1;
+`
 const VerantwortlichDropdown = styled(FormControl)`
   font-family: 'Lucida Console', Monaco, monospace;
   width: 80px;
 `
 
-const enhance = compose(inject('store'), observer)
+const AreaZuletztMutiert = ({
+  values,
+  change,
+  firstTabIndex,
+  changeComparator,
+}) => {
+  const store = useContext(storeContext)
 
-const AreaZuletztMutiert = ({ store, values, change, firstTabIndex, changeComparator }) => (
-  <Container>
-    <FieldVerantwortlichSelector>
-      <ControlLabel>Mutations-Person</ControlLabel>
-      <InputGroup>
-        <SortSelector name="mutationsperson" />
-        <ComparatorSelector name="mutationsperson" changeComparator={changeComparator} />
-        <VerantwortlichDropdown
-          componentClass="select"
-          value={values.mutationsperson || ''}
-          name="mutationsperson"
-          onChange={change}
-          tabIndex={1 + firstTabIndex}
-        >
-          {interneOptionsList(store.geschaefte.interneOptions)}
-        </VerantwortlichDropdown>
-      </InputGroup>
-    </FieldVerantwortlichSelector>
-    <FieldVerantwortlichName>{interneData(values, store.geschaefte.interneOptions)}</FieldVerantwortlichName>
-  </Container>
-)
+  return (
+    <Container>
+      <FieldVerantwortlichSelector>
+        <ControlLabel>Mutations-Person</ControlLabel>
+        <InputGroup>
+          <SortSelector name="mutationsperson" />
+          <ComparatorSelector
+            name="mutationsperson"
+            changeComparator={changeComparator}
+          />
+          <VerantwortlichDropdown
+            componentClass="select"
+            value={values.mutationsperson || ''}
+            name="mutationsperson"
+            onChange={change}
+            tabIndex={1 + firstTabIndex}
+          >
+            {interneOptionsList(store.geschaefte.interneOptions)}
+          </VerantwortlichDropdown>
+        </InputGroup>
+      </FieldVerantwortlichSelector>
+      <FieldVerantwortlichName>
+        {interneData(values, store.geschaefte.interneOptions)}
+      </FieldVerantwortlichName>
+    </Container>
+  )
+}
 
 AreaZuletztMutiert.displayName = 'AreaZuletztMutiert'
 
 AreaZuletztMutiert.propTypes = {
-  store: PropTypes.object.isRequired,
   values: PropTypes.object,
   firstTabIndex: PropTypes.number.isRequired,
   change: PropTypes.func.isRequired,
   changeComparator: PropTypes.func.isRequired,
 }
 
-export default enhance(AreaZuletztMutiert)
+export default observer(AreaZuletztMutiert)
