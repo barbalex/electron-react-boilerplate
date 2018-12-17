30a873f0c184fa842c08c707a4747c22a684b400
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-17 18:18:44 +0100

    use hooks

--- app/components/FilterFields/ComparatorSelector.js
+++ app/components/FilterFields/ComparatorSelector.js
@@ -1,11 +1,10 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import PropTypes from 'prop-types'
 import { InputGroup, FormControl } from 'react-bootstrap'
-import { observer, inject } from 'mobx-react'
-import compose from 'recompose/compose'
+import { observer } from 'mobx-react'
 import styled from 'styled-components'
 
-const enhance = compose(inject('store'), observer)
+import storeContext from '../../storeContext'
 
 const StyledFormControl = styled(FormControl)`
   width: 55px !important;
@@ -17,13 +16,20 @@ const StyledFormControl = styled(FormControl)`
   font-size: 14px !important;
 `
 
-const ComparatorSelector = ({ store, name, changeComparator }) => {
-  const filterField = store.geschaefte.filterFields.find(ff => ff.field === name)
+const ComparatorSelector = ({ name, changeComparator }) => {
+  const store = useContext(storeContext)
+  const filterField = store.geschaefte.filterFields.find(
+    ff => ff.field === name,
+  )
   const comparatorValue = filterField ? filterField.comparator : ''
 
   return (
     <InputGroup.Button>
-      <StyledFormControl componentClass="select" onChange={e => changeComparator(e.target.value, name)} value={comparatorValue}>
+      <StyledFormControl
+        componentClass="select"
+        onChange={e => changeComparator(e.target.value, name)}
+        value={comparatorValue}
+      >
         <option value="" />
         <option value="=">&#8776;</option>
         <option value="===">=</option>
@@ -39,9 +45,8 @@ const ComparatorSelector = ({ store, name, changeComparator }) => {
 ComparatorSelector.displayName = 'ComparatorSelector'
 
 ComparatorSelector.propTypes = {
-  store: PropTypes.object.isRequired,
   name: PropTypes.string.isRequired,
   changeComparator: PropTypes.func.isRequired,
 }
 
-export default enhance(ComparatorSelector)
+export default observer(ComparatorSelector)
