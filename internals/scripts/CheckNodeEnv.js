6275a6f6326cd5306f772ed75cfa06255dc2136f
Alexander Gabriel <alex.barbalex@gmail.com>
2018-12-20 11:35:06 +0100

    enable eslint server to run

--- internals/scripts/CheckNodeEnv.js
+++ internals/scripts/CheckNodeEnv.js
@@ -11,6 +11,10 @@ export default expectedEnv => {
         `"process.env.NODE_ENV" must be "${expectedEnv}" to use this webpack config`,
       ),
     )
-    process.exit(2)
+    /**
+     * For unknown reason this happens in dev mode and shuts down the eslint server
+     * see: https://github.com/Microsoft/vscode-eslint/issues/590#issuecomment-448943357
+     */
+    //process.exit(2)
   }
 }
